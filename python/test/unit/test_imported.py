70163a11a0be642ecf9887eb094e4cbf16f9ba27
0x26res <0x26res@gmail.com>
2025-12-24 17:58:25 +0000

    type wip

--- python/test/unit/test_imported.py
+++ python/test/unit/test_imported.py
@@ -1,12 +1,13 @@
-from ptars import HandlerPool
+from google.protobuf.descriptor import Descriptor
 from ptars.internal import _get_dependencies
-
+from typing import cast
+from ptars import HandlerPool
 from ptars_protos import bench_pb2, imported_pb2, importer_pb2, simple_pb2
 
 
 def test_with_imported_proto():
     handler = HandlerPool([importer_pb2.DESCRIPTOR]).get_for_message(
-        importer_pb2.ImporterMessage.DESCRIPTOR
+        cast(Descriptor, importer_pb2.ImporterMessage.DESCRIPTOR)
     )
     handler.list_to_record_batch(
         [
@@ -14,7 +15,8 @@ def test_with_imported_proto():
             for m in [
                 importer_pb2.ImporterMessage(
                     imported_message=imported_pb2.ImportedMessage(
-                        string_value="SYM1", imported_enum=1
+                        string_value="SYM1",
+                        imported_enum=imported_pb2.ImportedEnum.CORE_ENUM_FOO,
                     ),
                 )
             ]
--- scripts/protoc.py
+++ scripts/protoc.py
@@ -4,12 +4,13 @@ from typing import List
 
 import google.type.date_pb2
 import grpc_tools
+import importlib.resources
 
 _ROOT_DIR = pathlib.Path(__file__).parent.parent.absolute()
 _GOOGLE_COMMON_PROTOS_ROOT_DIR = pathlib.Path(
     google.type.date_pb2.__file__
-).parent.parent.parent.absolute()
-_GRPC_PROTOS_INCLUDE = pathlib.Path(grpc_tools.__file__).parent.absolute() / "_proto"
+).parents[2].absolute()
+_GRPC_PROTOS_INCLUDE = importlib.resources.files(grpc_tools) / "_proto"
 _SRC_DIR = _ROOT_DIR / "protos"
 _OUT_DIR = _ROOT_DIR / "ptars_protos"
 
